7bb0736a260520956b35e88f851061a8c3150868
sinzunza93 <sergio23@DESKTOP-8NSSR9D.localdomain>
2021-05-16 14:50:30 -0700

    change user field to uid

--- posts.js
+++ posts.js
@@ -7,7 +7,7 @@ var firebase = require('firebase/app');
 function postCreatePost(request, response){
 
     firebase.database().ref('Posts/' + request.body.postID).set({
-        user: request.body.user,
+        user: firebase.auth().currentUser.uid,
         imageURL: request.body.imageURL,
         category: request.body.category,
         date: Date.now(),
