af9c5fc4009cce6179249480518460c83bd96fbd
Ernie1056 <ernie6105@gmail.com>
2021-06-10 18:38:25 -0700

    User register working

--- server/user.js
+++ server/user.js
@@ -38,27 +38,38 @@ function postCreateUser(request, response){
 
     var email = request.body.email;
     var password = request.body.password;
-    
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-      .then((userCredential) => {
-        // Signed in 
-        var user = userCredential.user;
-        var rootRef = firebase.database().ref();
-        var userRef = rootRef.child('Users/' + firebase.auth().currentUser.uid);
-        userRef.set({
-          UserName: request.body.userName,
-          ProfilePic: "https://firebasestorage.googleapis.com/v0/b/photoit110.appspot.com/o/profilePhotos%2FdefaultProfilePhoto.png?alt=media&token=3a7c1bcd-7e36-404f-b7b8-3438549c4885"
-        });
-        console.log("create user successful");
-        response.send("successful");
-      })
-      .catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log("Error " + errorCode + ": " + errorMessage + ".\n");
-        response.send("unsuccessful");
-      });
-
+    var ref = firebase.database().ref('Users'); //.ref(target); 
+    var userName = request.body.userName; 
+    ref.orderByChild("UserName").equalTo(userName).once("value", function(snapshot){
+      if (snapshot.numChildren() >= 1)
+      {
+          // duplicate userName
+          response.send("User Name Already Exists");
+      }
+      else 
+      {
+          firebase.auth().createUserWithEmailAndPassword(email, password)
+          .then((userCredential) => {
+            // Signed in 
+            var user = userCredential.user;
+            var rootRef = firebase.database().ref();
+            var userRef = rootRef.child('Users/' + firebase.auth().currentUser.uid);
+            userRef.set({
+              UserName: request.body.userName,
+              ProfilePic: "https://firebasestorage.googleapis.com/v0/b/photoit110.appspot.com/o/profilePhotos%2FdefaultProfilePhoto.png?alt=media&token=3a7c1bcd-7e36-404f-b7b8-3438549c4885", 
+              AppreciatedPoint: 0
+            });
+            console.log("create user successful");
+            response.send("successful");
+          })
+          .catch((error) => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log("Error " + errorCode + ": " + errorMessage + ".\n");
+            response.send("unsuccessful");
+          });
+      }
+    }); 
 }
 
 function postSignIn(request, response){
