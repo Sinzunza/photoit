fc6b27a076eb69d559299bb41c2ef72fd5d3deed
Sinzunza <sergioinz01@gmail.com>
2021-09-23 21:44:14 -0700

    added upload profile photo feature

--- scripts/user.js
+++ scripts/user.js
@@ -2,6 +2,19 @@ window.onload = function() {
     getUserAuthentication();
 }
 
+var firebaseConfig = {
+    apiKey: "AIzaSyCOetUOyMKkAt_P_9xUWkcRB8J5ernXM10",
+    authDomain: "photoit110.firebaseapp.com",
+    databaseURL: "https://photoit110-default-rtdb.firebaseio.com",
+    projectId: "photoit110",
+    storageBucket: "photoit110.appspot.com",
+    messagingSenderId: "17838263350",
+    appId: "1:17838263350:web:b780ca82fda263eb549c97",
+    measurementId: "G-VC0GJ2GXLT"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 var loggedIn = false;
 
 function getUserAuthentication() {
@@ -74,18 +87,68 @@ function userState(){
     }
 }
 
+function chooseFile(e) {
+
+    var file = {};
+    file = e.target.files[0];
+
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+      $('#profilePic')
+        .attr('src', e.target.result)
+        .width(175)
+        .height(175);
+    };
+
+    reader.readAsDataURL(file);
+
+    // add confirm changes or cancel changes button
+
+   // uploadPicture(file);
+
+}
+
+function uploadPicture(file) {
+
+    var postStorageID = create_UUID(); 
+    var storageRef = firebase.storage().ref("profilePhotos/" + postStorageID);
+
+    storageRef.put(file).then(function() {
+            
+        console.log("upload successful");
+        
+        // uploaded, get URL
+        firebase.storage().ref('profilePhotos/' + postStorageID).getDownloadURL().then(imgUrl => {
+            
+            //add url to firebase db
+
+        });
+
+    }).catch(error => {
+        console.log("upload failed");
+        console.log("Error: " + error);
+    })
+
+}
+
 function getUserInfo(userIDUser) {
+
     var xhttp = new XMLHttpRequest();
     xhttp.responseType = 'json';
+
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) { // call is complete and call is successful
             var result = this.response;            
+            var profilePic = document.getElementById("profilePic");
             var userName = document.getElementById("user_name");
             var numLikes = document.getElementById("num_likes");
             var awards = document.getElementById("awards");
 
+            profilePic.src = result.ProfilePic;
             userName.innerHTML += result.Username;
             numLikes.innerHTML += result.AppreciationPoints;
+            
             if (result.awards != null) {
                 awards.innerHTML += result.awards;
             }
@@ -94,7 +157,9 @@ function getUserInfo(userIDUser) {
 
         }
     };
+
     var params = "?userID=" + userIDUser;
+
     xhttp.open("GET", "http://localhost:8081/GetUserInfo" + params, true);
     xhttp.setRequestHeader("Content-Type", "application/json");
     
@@ -148,4 +213,14 @@ function getUsersPosts(usernameUser) {
     xhttp.setRequestHeader("Content-Type", "application/json");
     
     xhttp.send();
+}
+
+function create_UUID(){
+    var dt = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (dt + Math.random()*16)%16 | 0;
+        dt = Math.floor(dt/16);
+        return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+    });
+    return uuid;
 }
