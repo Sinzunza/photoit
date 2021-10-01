e57d5ac28e25e69bc9576e45d70876d9fff3da85
Sinzunza <sergioinz01@gmail.com>
2021-10-01 12:06:04 -0700

    change output on upload when not signed in

--- scripts/upload.js
+++ scripts/upload.js
@@ -1,3 +1,14 @@
+var userStateBtn;
+var loggedIn;
+
+window.onload = function() {
+
+    userStateBtn = document.getElementById("userStateBtn");
+    getUserAuthentication();
+
+}
+
+
 var firebaseConfig = {
     apiKey: "AIzaSyCOetUOyMKkAt_P_9xUWkcRB8J5ernXM10",
     authDomain: "photoit110.firebaseapp.com",
@@ -22,6 +33,112 @@ function chooseFile(e) {
     else{
         docFileName.innerHTML = (file.name).substring(0,4) + "..." +(file.name).substring((file.name).length-8, (file.name).length);
     }
+}
+
+function getUserAuthentication() {
+    
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "http://localhost:8081/GetUserAuthentication", true);
+    xhttp.setRequestHeader("Content-Type", "application/json");
+    xhttp.send();
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) { // call is complete and call is successful
+
+            var result = this.response;
+
+            var category_infoDiv= document.getElementById("category_info");
+
+            if (result) {
+
+                console.log("\n\n HERE NOT SIGNED IN\n\n");
+
+                console.log("user signed in");
+                userStateBtn.innerHTML = "Sign Out";
+                loggedIn = true;
+                
+                category_infoDiv.innerHTML = "<b id=\"uploadText\" style=\"color:white;\">UPLOAD A PHOTO</b>" +
+                                              "<form id=\"uploadForm\" action=\"\">" +
+                                              "<ul class=\"flex-outer\">" + 
+                                              "<li>" +
+                                              "<p class=\"formItem\" style=\"color:white;\">Choose File:</p>" +
+                                              "<input type=\"file\" class=\"formItem hidden\" id=\"myFile\" accept=\"image/*\" title=\"\" value=\"\" name=\"filename\" onchange=\"chooseFile(event)\" style=\"color:white;display:none\">" +
+                                              "<label id=\"fileSelect\" for=\"myFile\" style=\"color: white;\">SELECT FILE</label>" +
+                                              "</li>" +
+                                              "<li><p class=\"formItem\" style=\"color:white;\">Title: <input type=\"text\" id=\"photoname\" name=\"Name\"></p></li>" +
+                                              "<li>" +
+                                              "<label for=\"categories\" class=\"formItem\" style=\"color:white;\">Choose a category:</label>" +
+                                              "<select id=\"categorymenu\" class=\"formItem\" name=\"categories\">" +
+                                              "<option value=\"Cars\">Cars</option>" +
+                                              "<option value=\"Food\">Food</option>" +
+                                              "<option value=\"Fashion\">Fashion</option>" +
+                                              "<option value=\"Funny\">Funny</option>" +
+                                              "<option value=\"Sports\">Sports</option>" +
+                                              "<option value=\"Science\">Science</option>" +
+                                              "<option value=\"Scenery\">Scenery</option>" +
+                                              "<option value=\"Tech\">Tech</option>" +
+                                              "<option value=\"Gaming\">Gaming</option>" +
+                                              "</select>" +
+                                              "</li>" +
+                                              "<li><button id=\"uploadButton\" class=\"formItem\" type=\"button\" onclick=\"uploadPicture()\">Upload</button></li>" +
+                                              "</ul>" +
+                                              "</form>";
+
+                getUserInfo(result);
+
+            }
+            else {
+
+                console.log("\n\n HERE NOT SIGNED IN\n\n");
+
+                console.log("user not signed in");
+                userStateBtn.innerHTML = "Login";
+                loggedIn = false;
+
+                category_infoDiv.innerHTML = "<p style=\"text-align: center; color: #fff;\"> You are not signed in! </p>";
+
+            }
+        }
+    };
+    
+}
+
+function userState() {
+
+    if(loggedIn){
+
+        userStateBtn.innerHTML = "Login";
+
+        // sign out the user
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("POST", "http://localhost:8081/SignOut", true);
+        xhttp.setRequestHeader("Content-Type", "application/json");
+        xhttp.send();
+
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) { // call is complete and call is successful
+                var result = this.response;
+                if (result == "true") {
+
+                    console.log("user signed out successfully");
+                    location.reload();
+
+                }
+                else {
+
+                    console.log("user sign out failed");
+
+                }
+            }
+        };
+
+    }
+    else {                                       
+
+        window.location.href = "../views/login.html";
+
+    }
+
 }
 
 function uploadPicture() {
